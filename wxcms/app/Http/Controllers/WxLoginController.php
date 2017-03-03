3f8931c6b76d0264bc3f767ef30a83990dfedb62
xiaoxuesheng <645615159@qq.com>
2017-03-03 14:36:22 +0800

    sd

--- wxcms/app/Http/Controllers/WxLoginController.php
+++ wxcms/app/Http/Controllers/WxLoginController.php
@@ -579,7 +579,7 @@ $a = json_decode($res,true);
               
                 $Request->session()->put('fd_id',$a['id']);
                 $Request->session()->put('status',1);
-                return redirect('fd_personal');
+                return redirect('zf_personal');
 
     }
 
--- wxcms/resources/views/static_wx/login.blade.php
+++ wxcms/resources/views/static_wx/login.blade.php
@@ -33,42 +33,8 @@
             </div>
             <button class="login_btn" type="submit">登&nbsp;&nbsp;录</button>
         </form>
-        <div class="other_login">
-            <div class="other"></div>
-            <span>其他方式登录</span>
-            <div class="other"></div>
-        </div>
-        <div class="other_choose">
-            <a onclick="toLogin()">
-                <img src="{{url('static_wx/img')}}/qq.png" alt=""/>
-            </a>
-            <a href="">
-                <img src="{{url('static_wx/img')}}/wx.png" alt=""/>
-            </a>
-            <a onclick="wbLogin()">
-                <img src="{{url('static_wx/img')}}/wb.png" alt=""/>
-            </a>
-        </div>
+        
     </div>
 </body>
-<script>
-    function toLogin()
-    {
-        //以下为按钮点击事件的逻辑。注意这里要重新打开窗口
-        //否则后面跳转到QQ登录，授权页面时会直接缩小当前浏览器的窗口，而不是打开新窗口
-//        var A=window.open("https://graph.qq.com/oauth2.0/authorize?response_type=token&client_id=101369412&redirect_uri=http://www.kjschool.net/api.php","TencentLogin",
-//                "width=450,height=320,menubar=0,scrollbars=1, resizable=1,status=1,titlebar=0,toolbar=0,location=1");
-    window.location.href='https://graph.qq.com/oauth2.0/authorize?response_type=token&client_id=101369412&redirect_uri=http://www.kjschool.net/api.php';
 
-    }
-    function wbLogin()
-    {
-        //以下为按钮点击事件的逻辑。注意这里要重新打开窗口
-        //否则后面跳转到QQ登录，授权页面时会直接缩小当前浏览器的窗口，而不是打开新窗口
-//        var A=window.open("https://graph.qq.com/oauth2.0/authorize?response_type=token&client_id=101369412&redirect_uri=http://www.kjschool.net/api.php","TencentLogin",
-//                "width=450,height=320,menubar=0,scrollbars=1, resizable=1,status=1,titlebar=0,toolbar=0,location=1");
-    window.location.href='https://api.weibo.com/oauth2/authorize?client_id=2887630111&redirect_uri=http://house.wlf928.cn/wblogin';
-
-    }
-</script>
 </html>
