c48bb96dd088296a3d00cd41d55b44e58403942d
xiaoxuesheng <645615159@qq.com>
2017-03-03 09:11:44 +0800

    asdfg

--- wxcms/app/Http/Controllers/WxLoginController.php
+++ wxcms/app/Http/Controllers/WxLoginController.php
@@ -573,7 +573,7 @@ $uid = $re['uid'];
 $urs = "https://api.weibo.com/2/users/show.json?access_token=$ken&uid=$uid";
 $res = file_get_contents($urs);
 $a = json_decode($res);
-var_dump($a['name']);
+var_dump($a);
                 // $Request->session()->put('fd_username',$a['name']);  //名称
                 // $Request->session()->put('r_toux',$a['profile_image_url']);  //头像
               
